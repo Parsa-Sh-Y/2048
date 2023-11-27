801096a6691ece952354c9f31b894024e934ee7c
Parsa-Sh-Y <p.shafiee.yekta@gmail.com>
2023-11-27 19:17:59 +0330

    Added initial files

---
+++ evaluation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+import game_functions as gf
+
+
+def evaluate_state(board: np.ndarray) -> float:
+    """
+    Returns the score of the given board state.
+    :param board: The board state for which the score is to be calculated.
+    :return: The score of the given board state.
+    """
+    # TODO: Complete evaluate_state function to return a score for the current state of the board
+    # Hint: You may need to use the np.nonzero function to find the indices of non-zero elements.
+    # Hint: You may need to use the gf.within_bounds function to check if a position is within the bounds of the board.
+    
+    raise NotImplementedError("Evaluation function not implemented yet.")
